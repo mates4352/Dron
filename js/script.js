49c0d1216a85cfb07eb1b2c0984fcf81ece455aa
Sergey <cabyrkih@mail.ru>
2021-01-18 16:59:09 +0300

    correction

--- js/script.js
+++ js/script.js
@@ -1,34 +1,20 @@
-/* ibg(вставляет задний фон через js) */
-function ibg() {
-
-	$.each($('.ibg'), function (index, val) {
-		if ($(this).find('img').length > 0) {
-			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
-		}
-	});
-}
-
-ibg();
-
-/* ibg(вставляет задний фон через js) */
 
 /* active(onclick) */
 
+function getClass(bt) {
+	let bth=document.querySelector('.header__burger')
+	let wrapper=document.querySelector('.wrapper');
 
-
-
-
-
-
-function getClass(bth) {
 	bth.onclick = function () {
-		this.classList.toggle("active")
+		bth.classList.toggle("active")
+		bt.classList.toggle("active")
+		wrapper.classList.toggle("lock")
 	}
-
 }
 getClass(
-	document.querySelector('.header__burger')
-);
+	document.querySelector('.menu')
+)
+
 
 
 // $(document).ready(function () {
@@ -41,34 +27,5 @@ getClass(
 // });
 
 
-/* adaptive(уберает указанный блок кода в другой родитель) */
-
-$(window).resize(function (event) {
-	adaptive_function();
-});
-
-function adaptive_header(w, h) {
-	var headerMenu = $('.menu');
-	var header__menu = $('.nav__list');
-
-	if (w < 1200) {
-		if (!header__menu.hasClass('done')) {
-			header__menu.addClass('done').appendTo(headerMenu);
-		}
-
-	} else {
-		if (header__menu.hasClass('done')) {
-			header__menu.removeClass('done').prependTo($('.header__nav'));
-		}
-	}
-}
-function adaptive_function() {
-	var w = $(window).outerWidth();
-	var h = $(window).outerHeight();
-	adaptive_header(w, h);
-}
-adaptive_function();
-
-/* adaptive(уберает указанный блок кода в другой родитель) */
 
 
